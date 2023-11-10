a85c194dd4ab6391773b7ad2450656352b0a0abf
Žiga Miklošič <ziga.miklosic@seltron.si>
2023-11-10 15:07:28 +0100

    - added maping table for label and textline widget

--- projects/house_market_calculator/HouseMarketCalculator/house_market.cpp
+++ projects/house_market_calculator/HouseMarketCalculator/house_market.cpp
@@ -23,7 +23,9 @@ Widget::Widget(QWidget *parent)
     connect( ui->renta,     &QLineEdit::textChanged, this, &Widget::InputFormatter );
 
 
-    connect( ui->sklad,     &QLineEdit::textChanged, this, &Widget::AnnualExpenseUpdate );
+    connect( ui->sklad,         &QLineEdit::textChanged, this, &Widget::AnnualExpenseUpdate );
+    connect( ui->zavarovanje,   &QLineEdit::textChanged, this, &Widget::AnnualExpenseUpdate );
+    connect( ui->poloznice,     &QLineEdit::textChanged, this, &Widget::AnnualExpenseUpdate );
 
 
 }
@@ -32,6 +34,9 @@ Widget::~Widget()
 {
     delete ui;
 }
+
+
+
 
 void Widget::InputFormatter(const QString &new_text)
 {
@@ -46,15 +51,52 @@ void Widget::InputFormatter(const QString &new_text)
 
 
 
-
+#include <QMap>
 
 void Widget::AnnualExpenseUpdate(const QString &new_value)
 {
+
+    QHash<QLineEdit*,QLabel*> map;
+
+    map.insert( ui->sklad,          ui->sklad_leto );
+    map.insert( ui->zavarovanje,    ui->zavarovanje_leto );
+    map.insert( ui->poloznice,      ui->poloznice_leto );
+
+
     // Get pointer to line edit object that triggers that function
-    //QLineEdit * p_LineEdit = static_cast<QLineEdit*> ( this->sender() );
+    QLineEdit * p_LineEdit = static_cast<QLineEdit*> ( this->sender() );
+
+
+    for ( auto [key, value] : map.asKeyValueRange())
+    {
+        if ( key == p_LineEdit )
+        {
+            value->setText( QString::number( new_value.toInt() * 12 ) + " €/leto" );
+            break;
+        }
+    }
+
+#if 0
+
+        QHash<QString, int> hash;
+    hash.insert("January", 1);
+    hash.insert("February", 2);
+    // ...
+    hash.insert("December", 12);
+
+    for (auto [key, value] : hash.asKeyValueRange()) {
+        cout << qPrintable(key) << ": " << value << endl;
+        --value; // convert to JS month indexing
+    }
+
+
+    qInfo() << "Line Edit: " << p_LineEdit->objectName();
+
+
 
 
     ui->sklad_leto->setText( QString::number( new_value.toInt() * 12 ) + " €/leto" );
+#endif
 }
 
 
